61e566f3449fee080cc317c0bbcf10ba8bfb2540
Julio-Freitas <julio.clf31@gmail.com>
2022-11-20 18:21:53 -0300

    fix: adjustment config

---
+++ src/styles/reset.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    // this is the shared style
+  html {
+    box-sizing: border-box;
+    font-size: 84.5%
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    
+  }
+
+
+
+  // anything else you would like to include
+`;
